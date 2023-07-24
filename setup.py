0600da34ecd56aef4cc7b9a1d7e2ef4d47170600
Olusegun O <50618702+segestic@users.noreply.github.com>
2023-07-24 15:16:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mfrc522",
-    version="0.0.7",
+    name="mfrc522-SS",
+    version="0.0.1",
     author="Pi My Life Up",
     author_email="support@pimylifeup.com",
     description="A library to integrate the MFRC522 RFID readers with the Raspberry Pi",
